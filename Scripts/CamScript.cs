1a8a0f18646045d6690de87f0f18edc1e55ba547
Tim Shmidt <timsh007@gmail.com>
2025-07-01 17:12:46 -0700

    made a camera script

---
+++ Scripts/CamScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamScript : MonoBehaviour {
+
+    private Vector3 offset;
+    [SerializeField] private GameObject objToFollow;
+
+    [SerializeField] private int minO = 1; //min for ortho
+    [SerializeField] private int maxO = 100; //max for ortho
+    [SerializeField] private int minP = 10; //min for perspective
+    [SerializeField] private int maxP = 90; //max for perspective
+
+    void Start() {
+        offset = transform.position - objToFollow.transform.position;
+        offset = new Vector3(0, 0, offset.z);
+
+        Camera camera = gameObject.GetComponent<Camera>();
+    }
+
+    void Update() {
+        transform.position = objToFollow.transform.position + offset;
+
+        Camera camera = gameObject.GetComponent<Camera>();
+        if (camera.orthographic) {
+            camera.orthographicSize -= Input.mouseScrollDelta.y; //adds mouse scroll to cam size
+            if (camera.orthographicSize > maxO) { //makes cam size unable to go above max
+                camera.orthographicSize = maxO;
+            }
+            if (camera.orthographicSize < minO) { //makes cam size unable to go below min
+                camera.orthographicSize = minO;
+            }
+        } else {
+            camera.fieldOfView -= 2 * Mathf.Atan(Input.mouseScrollDelta.y); //adds mouse scroll to cam fov, tangent for smooth cam movement
+            if (camera.fieldOfView > maxP) { //makes cam size unable to go above max
+                camera.fieldOfView = maxP;
+            }
+            if (camera.fieldOfView < minP) { //makes cam size unable to go below min
+                camera.fieldOfView = minP;
+            }
+        }
+    }
+}
