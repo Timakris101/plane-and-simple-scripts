5177cbec13614efb7b91b96d49b5435b5f61a5aa
Tim Shmidt <timsh007@gmail.com>
2025-07-02 10:06:10 -0700

    added a "rollover"

--- Scripts/PlaneController.cs
+++ Scripts/PlaneController.cs
@@ -22,13 +22,18 @@ public class PlaneController : MonoBehaviour {
     [SerializeField] private AnimationCurve torqueStrength;
     [SerializeField] private float baseTorque;
     [SerializeField] private float alignmentStrength;
-    [SerializeField] private float alignmentThresh = 45f;
+    [SerializeField] private float alignmentThresh;
+
+    [Header("Roll")]
+    [SerializeField] private float rollOverThresh;
 
     [Header("Atmosphere")]
     [SerializeField] private float airDensity;
 
     void Update() {
         handleControls();
+        if (AoA() < -rollOverThresh / GetComponent<Rigidbody2D>().velocity.magnitude) transform.RotateAround(transform.position, transform.right, 180f);
+        GetComponent<Rigidbody2D>().centerOfMass = transform.Find("CoM").localPosition;
     }
 
     void FixedUpdate() {
@@ -41,7 +46,7 @@ public class PlaneController : MonoBehaviour {
     private float AoA() {
         Vector2 velocity = GetComponent<Rigidbody2D>().velocity;
         if (velocity.magnitude < 1f) return 0;
-        return Vector2.SignedAngle(velocity, transform.right);
+        return Vector3.SignedAngle(velocity, transform.right, transform.forward);
     }
 
     private void handleThrust() {
@@ -54,8 +59,8 @@ public class PlaneController : MonoBehaviour {
 
     private void handleLift() {
         float liftForce = cL.Evaluate(AoA()) * airDensity * Mathf.Pow(GetComponent<Rigidbody2D>().velocity.magnitude, 2) * wingArea / 2f;
-        Vector2 liftDir = Vector2.Perpendicular(GetComponent<Rigidbody2D>().velocity).normalized;
-        GetComponent<Rigidbody2D>().AddForce(liftDir * liftForce);
+        Vector2 liftDir = Vector3.Cross(GetComponent<Rigidbody2D>().velocity, -transform.forward).normalized;
+        GetComponent<Rigidbody2D>().AddForceAtPosition(liftDir * liftForce, transform.Find("CoL").position);
     }
 
     private void handleDrag() {
@@ -83,8 +88,19 @@ public class PlaneController : MonoBehaviour {
 
     private void handleTorque() {
         int dirToTurn = getWantedDir();
-        GetComponent<Rigidbody2D>().angularVelocity += dirToTurn * torqueStrength.Evaluate(GetComponent<Rigidbody2D>().velocity.magnitude) * baseTorque * Time.fixedDeltaTime;
-        if (GetComponent<Rigidbody2D>().velocity.magnitude > 1f && (AoA() > alignmentThresh || AoA() < -alignmentThresh)) GetComponent<Rigidbody2D>().angularVelocity += -AoA() * alignmentStrength * torqueStrength.Evaluate(GetComponent<Rigidbody2D>().velocity.magnitude) * Time.fixedDeltaTime;
+        if (dirToTurn == 0 && torqueStrength.Evaluate(GetComponent<Rigidbody2D>().velocity.magnitude) > .1f) {
+            GetComponent<Rigidbody2D>().angularVelocity /= 1 + torqueStrength.Evaluate(GetComponent<Rigidbody2D>().velocity.magnitude);
+            return;
+        }
+        bool realignNeeded = GetComponent<Rigidbody2D>().velocity.magnitude > 1f && (AoA() > alignmentThresh || AoA() < -alignmentThresh);
+        if (dirToTurn == 0 && !realignNeeded) {
+            return;
+        }
+        if (torqueStrength.Evaluate(GetComponent<Rigidbody2D>().velocity.magnitude) < .1f && !realignNeeded) {
+            return;
+        }
+        GetComponent<Rigidbody2D>().angularVelocity = dirToTurn * torqueStrength.Evaluate(GetComponent<Rigidbody2D>().velocity.magnitude) * baseTorque;
+        if (realignNeeded) GetComponent<Rigidbody2D>().angularVelocity = -AoA() * alignmentStrength * torqueStrength.Evaluate(GetComponent<Rigidbody2D>().velocity.magnitude);
     }
 
     private void handleControls() {
