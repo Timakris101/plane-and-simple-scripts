5f0c77ae59f2634fa4450b34e899ca01aa925043
Tim Shmidt <timsh007@gmail.com>
2025-07-03 15:20:41 -0700

    added damage model and bullet logic

--- Scripts/BulletScript.cs
+++ Scripts/BulletScript.cs
@@ -3,8 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletScript : MonoBehaviour {
-
+    [Header("Base Stats")]
     [SerializeField] private float initSpeed;
+    [SerializeField] private float damage;
+    [SerializeField] private float explosionRad;
+    
+
+    void OnCollisionEnter2D() {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, Mathf.Max(explosionRad, 0.5f));
+        foreach (Collider2D hit in hits) {
+            if (hit.transform.GetComponent<DamageModel>() != null) {
+                hit.transform.GetComponent<DamageModel>().hit(Random.Range(damage / 2f, damage));
+            }
+        }
+        Destroy(gameObject);
+    }
 
     void Start() {
         
---
+++ Scripts/DamageModel.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageModel : MonoBehaviour {
+
+    [SerializeField] private float hitChance;
+    [SerializeField] private string[] hitEffects;
+    [SerializeField] private float maxHealth;
+    [SerializeField] private float health;
+
+    private Aerodynamics aero;
+
+    void Start() {
+        health = maxHealth;
+        aero = transform.parent.GetComponent<Aerodynamics>();
+    }
+
+    void Update() {
+        
+    }
+
+    public void hit(float damage) {
+        if (Random.Range(0f, 1f) < hitChance) {
+            applyEffects(damage);
+        }
+    }
+
+    private void applyEffects(float damage) {
+        health -= damage;
+        foreach (string effect in hitEffects) {
+            if (effect == "tail") {
+                aero.setBaseTorque(health <= 0 ? 0 : aero.getBaseTorque() * (1 - damage / maxHealth));
+            }
+            if (effect == "wings") {
+                aero.setWingArea(health <= 0 ? 0 : aero.getWingArea() * (1 - damage / maxHealth));
+                if (health / maxHealth > Random.Range(0f, .5f)) {
+                    if (transform.parent.Find("Gear") != null) transform.parent.Find("Gear").GetComponent<GearScript>().breakGear();
+                }
+                if (health / maxHealth > Random.Range(.5f, .75f)) {
+                    if (transform.parent.Find("Flaps") != null) transform.parent.Find("Flaps").GetComponent<FlapScript>().breakFlaps();
+                }
+            }
+            if (effect == "engine") {
+                aero.setMaxThrust(health <= 0 ? 0 : aero.getMaxThrust() - damage / maxHealth);
+            }
+            if (effect == "pilot") {
+                if (health == 0) {
+                    aero.setBaseTorque(0);
+                    aero.setAlignmentThresh(0);
+                }
+            }
+        }
+    }
+}
