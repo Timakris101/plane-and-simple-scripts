4d9ac32a702baf848e4f4cf7f77a53d58128cb14
Tim Shmidt <timsh007@gmail.com>
2025-07-17 10:11:00 -0700

    made terrain generate from maxheight/2 to maxheight

--- Scripts/TerrainGen.cs
+++ Scripts/TerrainGen.cs
@@ -11,7 +11,7 @@ public class TerrainGen : MonoBehaviour {
         Vector2[] terrainVecs = new Vector2[(int) terrainPointAmt];
         GetComponent<SpriteShapeController>().spline.Clear();
         for (int i = 0; i < terrainPointAmt; i++) {
-            terrainVecs[i] = new Vector2(-(terrainLength / 2) + i * (terrainLength / terrainPointAmt), Random.Range(0f, maxHeight));
+            terrainVecs[i] = new Vector2(-(terrainLength / 2) + i * (terrainLength / terrainPointAmt), Random.Range(maxHeight / 2f, maxHeight));
             GetComponent<SpriteShapeController>().spline.InsertPointAt(i, new Vector2(terrainVecs[i].x, i == 0 || i == terrainPointAmt - 1 ? 0f : terrainVecs[i].y));
         }
         terrainVecs[0] = new Vector2(-terrainLength / 2, 0f);
