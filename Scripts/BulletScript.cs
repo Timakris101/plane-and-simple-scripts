6468d3197fe4a4867ca7671df7988b5bdd975b57
Tim Shmidt <timsh007@gmail.com>
2025-07-17 21:03:11 -0700

    made bullets not hit the plane that fired them

--- Scripts/BulletScript.cs
+++ Scripts/BulletScript.cs
@@ -9,15 +9,22 @@ public class BulletScript : MonoBehaviour {
     [SerializeField] private float explosionRad;
     [SerializeField] private float penetrationVal;
     
+    [Header("Plane")]
+    [SerializeField] private GameObject planeFired;
 
     void OnCollisionEnter2D(Collision2D col) {
         RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, explosionRad == 0 ? transform.localScale.x : explosionRad, col.relativeVelocity, penetrationVal);
         foreach (RaycastHit2D hit in hits) {
+            if (hit.transform.gameObject == planeFired) continue;
             if (hit.collider.transform.GetComponent<DamageModel>() != null) {
                 hit.collider.transform.GetComponent<DamageModel>().hit(Random.Range(damage / 2f, damage));
             }
         }
         Destroy(gameObject);
+    }
+
+    public void setPlaneFired(GameObject plane) {
+        planeFired = plane;
     }
 
     void Start() {
--- Scripts/GunScript.cs
+++ Scripts/GunScript.cs
@@ -20,11 +20,12 @@ public class GunScript : MonoBehaviour {
         GameObject newBullet = Instantiate(bullet, transform.position, transform.rotation);
         Vector3 baseVel = transform.parent.GetComponent<Rigidbody2D>() == null ? transform.parent.parent.GetComponent<Rigidbody2D>().velocity : transform.parent.GetComponent<Rigidbody2D>().velocity; //looks up hierarchy by 2
         newBullet.GetComponent<Rigidbody2D>().velocity = newBullet.GetComponent<BulletScript>().getInitSpeed() * transform.right + baseVel;
+        newBullet.GetComponent<BulletScript>().setPlaneFired(transform.parent.GetComponent<Aerodynamics>() == null ? transform.parent.parent.gameObject : transform.parent.gameObject);
     }
 
     void Update() {
         timer += Time.deltaTime;
-        if (timer > fireRate && shooting) {
+        if (timer > fireRate && shooting && ammunition > 0) {
             timer = 0;
             shoot();
         }
