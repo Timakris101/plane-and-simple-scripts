0423e1afa864595695095c456831ab1411209e2f
Tim Shmidt <timsh007@gmail.com>
2025-08-13 16:38:12 -0700

    improved finding of base vel to make it rely on change in position rather than rigidbodies

--- Scripts/GunScript.cs
+++ Scripts/GunScript.cs
@@ -11,6 +11,8 @@ public class GunScript : MonoBehaviour {
     [SerializeField] protected int ammunition;
     [SerializeField] private float bulletFuse;
     private bool shooting;
+    private Vector3 prevPos;
+    private Vector3 baseVel;
 
     protected void Start() {
         ammunition = maxAmmunition;
@@ -19,7 +21,6 @@ public class GunScript : MonoBehaviour {
     protected virtual void shoot() {
         ammunition--;
         GameObject newBullet = Instantiate(bullet, transform.position, transform.rotation);
-        Vector3 baseVel = transform.parent.GetComponent<Rigidbody2D>() == null ? transform.parent.parent.GetComponent<Rigidbody2D>().velocity : transform.parent.GetComponent<Rigidbody2D>().velocity; //looks up hierarchy by 2
         newBullet.GetComponent<Rigidbody2D>().velocity = newBullet.GetComponent<BulletScript>().getInitSpeed() * transform.right + baseVel;
         newBullet.GetComponent<BulletScript>().setPlaneFired(transform.parent.GetComponent<Aerodynamics>() == null ? transform.parent.parent.gameObject : transform.parent.gameObject);
         newBullet.GetComponent<BulletScript>().setFuseTime(bulletFuse);
@@ -31,6 +32,11 @@ public class GunScript : MonoBehaviour {
             timer = 0;
             shoot();
         }
+    }
+
+    private void FixedUpdate() {
+        baseVel = (transform.position - prevPos) / Time.fixedDeltaTime;
+        prevPos = transform.position;
     }
 
     public void setFuseOfBullets(float sec) {
