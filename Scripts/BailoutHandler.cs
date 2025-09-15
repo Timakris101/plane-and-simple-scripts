0e28580e5d1f5b8e153e10f5e5024e57e3816ff2
Tim Shmidt <timsh007@gmail.com>
2025-09-14 19:34:06 -0700

    fixed bailouthandler to work with the fact that the hitboxes' transforms are at a relative 0 0 0

--- Scripts/BailoutHandler.cs
+++ Scripts/BailoutHandler.cs
@@ -33,22 +33,21 @@ public class BailoutHandler : MonoBehaviour {
         for (int i = 0; i < transform.childCount; i++) {
             if (transform.GetChild(i).GetComponent<DamageModel>() == null) continue;
             if (!transform.GetChild(i).GetComponent<DamageModel>().isCrewRole()) continue;
+            if (!transform.GetChild(i).GetComponent<DamageModel>().isAlive()) continue;
 
-            if (transform.GetChild(i).GetComponent<DamageModel>().isAlive()) {
-                GameObject hitbox = transform.GetChild(i).gameObject;
+            GameObject hitbox = transform.GetChild(i).gameObject;
 
-                GameObject newCrew = Instantiate(crew, hitbox.transform.position, Quaternion.identity);
-                newCrew.GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity;
+            GameObject newCrew = Instantiate(crew, hitbox.transform.position + (Vector3) hitbox.GetComponent<BoxCollider2D>().offset, Quaternion.identity);
+            newCrew.GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity;
 
-                if (transform.Find("Camera") != null) transform.Find("Camera").parent = null;
+            if (transform.Find("Camera") != null) transform.Find("Camera").parent = null;
 
-                hitbox.GetComponent<BoxCollider2D>().size = newCrew.GetComponent<BoxCollider2D>().size;
-                hitbox.GetComponent<BoxCollider2D>().offset = newCrew.GetComponent<BoxCollider2D>().offset;
-                hitbox.transform.rotation = newCrew.transform.rotation;
-                hitbox.transform.parent = newCrew.transform;
-                
-                i--;
-            }
+            hitbox.GetComponent<BoxCollider2D>().size = newCrew.GetComponent<BoxCollider2D>().size;
+            hitbox.GetComponent<BoxCollider2D>().offset = newCrew.GetComponent<BoxCollider2D>().offset;
+            hitbox.transform.rotation = newCrew.transform.rotation;
+            hitbox.transform.parent = newCrew.transform;
+            
+            i--;
         }
     }
 }
